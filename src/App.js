9d3d497b9f423e4331351f30dc27084fd8a32656
Kosieun <sieun1156@naver.com>
2023-09-13 16:07:39 +0900

    react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/movie/:id" element={<Detail />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
